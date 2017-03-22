2919c518cfa34619ee8193172b61a6e1ab8815ca
luzhiqiang <listener_lzq@163.com>
2017-03-22 11:25:17 +0800

    modify project structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+import Start from './scenes/Start';
+import Home from './scenes/Home';
+import AuthorityApply from './scenes/AuthorityApply';
+import SystemConfig from './scenes/SystemConfig';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Text>App</Text>
+    )
+  }
+};
---
+++ src/scenes/SystemConfig.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  Button,
+} from 'react-native';
+
+export default class Start extends Component {
+  render() {
+    return (
+      <View>
+        <Text>SystemConfig</Text>
+      </View>
+    )
+  }
+}
